e795fb88a486734312ddc508b71efc448d1bc29b
PMacBeets <planktonlangton@gmail.com>
2023-10-03 16:55:43 +1100

    Now the front and backend are connected in a basic manner

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from backend.app.views.user_views import UserBase, User
 from backend.app.models.user import User as DBUser
 from backend.app.database import get_db
+from typing import List
 
 router = APIRouter()
 
@@ -25,3 +26,8 @@ def create_user(user: UserBase, db: Session = Depends(get_db)):
 
     db.refresh(db_user)
     return db_user
+
+@router.get("/", response_model=List[User])  # Ensure to import List from typing
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = db.query(DBUser).offset(skip).limit(limit).all()
+    return users
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from backend.app.database import engine, Base
 # Ensure the User model is imported before table creation
 from backend.app.controllers import user_controller
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
 
@@ -10,3 +13,15 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(user_controller.router, prefix="/users", tags=["users"])
 
+origins = [
+    "http://localhost:5173",  # Assuming your frontend runs on port 3000
+    # Add other origins (frontend deployments, etc.) if necessary
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
